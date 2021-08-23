120994c88e8b0ed52ad762b21019399974b393d4
Pratik Nanda <pratik.nanda@gmail.com>
2021-08-23 19:58:48 +0530

    Second Commit

--- src/com/greatlearning/project1/TechDepartment.java
+++ src/com/greatlearning/project1/TechDepartment.java
@@ -1,16 +1,20 @@
 package com.greatlearning.project1;
 
 public class TechDepartment extends SuperDepartment {
+	
+	@Override
 	public String departmentName() {
 		return "Tech Department ";
 
 	}
 
+	@Override
 	public String getTodaysWork() {
 		return "Complete coding of module 1";
 
 	}
 
+	@Override
 	public String getWorkDeadline() {
 		return "Complete by EOD ";
 
